25ce742745611a50d1564b0dd5674a520d6c9d7c
divya-2753 <divyab.mca2024@moonjeinstitute.com>
2026-05-07 05:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 
 app = Flask(__name__,
 template_folder='templates')
-app.secrete_key = 'supersecretkey123'
+app.secret_key = 'supersecretkey123'
 
 # 🔐 USERS (ROLE SYSTEM)
 USERS = {
