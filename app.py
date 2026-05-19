8d031e06646cd673eb250101e3974ce9b9fe583b
divya-2753 <divyab.mca2024@moonjeinstitute.com>
2026-05-19 17:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -898,9 +898,9 @@ def server_error(e):
     return render_template("login.html", error="Server Error! Admin ला contact करा."), 500
  
 # ================ RUN ================
- 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+ if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
